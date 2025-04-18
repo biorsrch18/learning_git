fa260de8ab6794c4050fb72f0d3e11d0d9eb538c
Mala Sharma <bio.rsrch18@gmail.com>
2025-04-18 15:52:53 -0500

    new command

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-def sum(a,b,c,d):
-    return a+b+c+d
+def sum(a,b):
+    return a+b
 
-def sub(a,b):
-    return a-b
+def sub(a,b,c):
+    return a-b-c
 
 def div(a,b):
     return a/b
+
+def square(a):
+    return a*a
